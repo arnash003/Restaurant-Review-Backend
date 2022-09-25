0f19fe00a7e57f795fc0661e2e86b5fd635070a4
Asher-jay Arenz <asher-jayarenz@Asher-jays-MacBook-Air.local>
2022-09-26 01:06:42 +0900

    listening on port 3000

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const port = process.env.PORT || 8000
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
-        poolSize: 50,
-        wtimeout: 2500,
-        useNewUrlParse: true
+        maxPoolSize:50,
+        wtimeoutMS:2500,
     }
 )
     .catch(err => {
